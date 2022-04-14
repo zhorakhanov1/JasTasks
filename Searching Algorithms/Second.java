19058f46afa068adde4291a108f9dc4a1fa0bb94
zhorakhanov1 <49300109+zhorakhanov1@users.noreply.github.com>
2022-04-14 12:05:24 +0600

    Update Second.java

--- Searching Algorithms/Second.java
+++ Searching Algorithms/Second.java
@@ -3,17 +3,28 @@ package com.company;
 public class Second {
 
     public static void main(String[] args) {
-        int z = search(new int[]{5, 6, 7, 8, 9, 10, 1, 2, 3}, 3);
-        if (z == -1) System.out.println("Not Found");
-        else System.out.println("Found at index: " + z);
+        int arr[] = {30, 40, 50, 10, 20};
+        int i = binarySearch(arr, 0, arr.length, 10);
+        if (i != -1) System.out.println("Index: " + i);
+        else System.out.println("Key not found");
     }
-    public static int search(int[] array, int a){
-        int res = 0;
-        for (int i = 0; i < array.length; i++){
-            if (array[i] == a){
-                res = i;
-                return i;
+    
+    public static int binarySearch(int arr[], int l, int r, int x) {
+        if (r >= l) {
+            int mid = (l + r) / 2;
+            if (arr[mid] == x) return mid;
+
+            if (arr[l] <= arr[mid]){
+                if (x >= arr[l] && x <= arr[mid])
+                    return binarySearch(arr, l, mid - 1, x);
+
+                return binarySearch(arr,mid+1,r,x);
             }
+
+            if (x >= arr[mid+1] && x <= arr[r]){
+                return binarySearch(arr,mid+1,r,x);
+            }
+            return binarySearch(arr,l,mid-1,x);
         }
         return -1;
     }
